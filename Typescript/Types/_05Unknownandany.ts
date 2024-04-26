5ee4b7e77f6c4f6c6f2b31718e6dade2a480283a
Raj143 <rajshingade777@gmail.com>
2024-04-26 11:55:33 +0530

    [Raj] add : done with the any and unknown anotation in typescript

---
+++ Typescript/Types/_05Unknownandany.ts
@@ -0,0 +1,32 @@
+let userInput: any;
+
+// Assume ki userInput ko user se kisi form mein input liya ja raha hai
+userInput = 5; // A valid number
+console.log(typeof userInput); // Output: number
+
+userInput = "Hello"; // A valid string
+console.log(typeof userInput); // Output: string
+
+userInput = { name: "John", age: 30 }; // A valid object
+console.log(typeof userInput); // Output: object
+
+// Ab aap dekh sakte hain ki userInput mein kuch bhi store kiya ja sakta hai.
+
+
+
+
+// Unknown 
+let userInput2: unknown;
+
+userInput2 = 5;
+if (typeof userInput2 === "number") {
+    let numberValue: number = userInput2; // Typecasting
+    console.log(numberValue * 2); // Output: 10
+}
+
+userInput2 = "Hello";
+if (typeof userInput2 === "string") {
+    let stringValue: string = userInput2; // Typecasting
+    console.log(stringValue.toUpperCase()); // Output: HELLO
+}
+//Here in case of unknown each time we are doing typecasting and that is must condition for unknown
