00cd5dde9a2199ad7f18de046d0f520ed6273af9
Raj143 <rajshingade777@gmail.com>
2024-04-26 15:16:39 +0530

    [Raj] add : adding the tuples file

---
+++ Typescript/_09Tuples.js
@@ -0,0 +1,15 @@
+// Tuple to represent employee details: id, name, age
+var employee = [1, "John Doe", 30];
+// Accessing tuple elements
+console.log(employee[0]); // Output: 1
+console.log(employee[1]); // Output: John Doe
+console.log(employee[2]); // Output: 30
+// Tuple to represent coordinates: x, y
+var point = [10, 20];
+// Accessing tuple elements
+console.log(point[0]); // Output: 10
+console.log(point[1]); // Output: 20
+//Most important thing while considering tuples is that type is fixed and 
+//Order of tuples must be considered and fixed number of elements....
+//
+// If we try to set elements in the wrong order then it will throw error
---
+++ Typescript/_09Tuples.ts
@@ -0,0 +1,20 @@
+// Tuple to represent employee details: id, name, age
+let employee: [number, string, number] = [1, "John Doe", 30];
+
+// Accessing tuple elements
+console.log(employee[0]); // Output: 1
+console.log(employee[1]); // Output: John Doe
+console.log(employee[2]); // Output: 30
+
+// Tuple to represent coordinates: x, y
+let point: [number, number] = [10, 20];
+
+// Accessing tuple elements
+console.log(point[0]); // Output: 10
+console.log(point[1]); // Output: 20
+
+//Most important thing while considering tuples is that type is fixed and 
+//Order of tuples must be considered and fixed number of elements....
+//
+// If we try to set elements in the wrong order then it will throw error
+
