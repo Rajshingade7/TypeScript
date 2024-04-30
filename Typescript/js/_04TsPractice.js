6a1fac203f84ba7d0770677a8193b1c6fb08e0c6
Raj143 <rajshingade777@gmail.com>
2024-04-30 17:20:34 +0530

    [Raj] add : adding all the imp files which are required

---
+++ Typescript/js/_04TsPractice.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.abc = void 0;
+exports.abc = "pqr";
+exports.abc = "zy";
+console.log(exports.abc);
---
+++ Typescript/src/_04TsPractice.ts
@@ -0,0 +1,4 @@
+export let abc:string="pqr";
+abc="zy";
+console.log(abc);
+
